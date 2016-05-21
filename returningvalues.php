25bdfeb2ec6e83b641f5a5efade1896b30b16a94
Kevin Wade <krwade1988@yahoo.com>
2016-05-21 16:00:06 -0400

    returning values arrays

--- returningvalues.php
+++ returningvalues.php
@@ -13,3 +13,33 @@ $greeting = hello("Mike");
 echo $greeting;
 
 ?>
+
+<?php
+
+function add_up($a, $b) {
+  return $a + $b;
+
+}
+
+
+$value = add_up(2, 4);
+
+echo $value;
+?>
+
+<?php
+
+function add_2up($a, $b) {
+	$arr = array (
+		$a,
+		$b,
+		$a + $b
+		);
+	return $arr;
+}
+
+$value = add_2up(2, 4);
+
+print_r($value);
+echo $value[2];
+?>
